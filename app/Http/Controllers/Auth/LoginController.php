b6f034f3e1e51fd78f168915a644442b22d08808
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2026-06-28 17:20:37 +0700

    mengubah login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,8 +4,48 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function handleLogin(Request $request)
+    {
+        $credentials = $request->validate(
+            [
+                'email' => 'required|email',
+                'password' => 'required'
+            ],
+            [
+                'email.required' => 'Email harus diisi',
+                'email.email' => 'Email tidak valid',
+                'password.required' => 'Password harus diisi',
+                'password.password' => 'Password tidak valid'
+            ]
+        );
+
+        if (Auth::attempt($credentials)) {
+            // dd('berhasil login');
+            $request->session()->regenerate();
+
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'Kredential yang anda masukan tidak sesuai dengan data kami.'
+        ])->onlyInput('email');
+    }
+
+    public function logout(Request $request)
+    {
+
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
 }
