0274d5ebfa050097305ab8c1f5e9ec60bc05c635
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2026-06-28 17:56:51 +0700

    migrate product

---
+++ database/migrations/2026_06_26_172228_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('kategori_id')->nullable()->constrained('kategoris')->nullOnDelete();
+            $table->string('nama_produk');
+            $table->string('kode_produk')->unique();
+            $table->integer('harga_jual');
+            $table->integer('harga_beli_pokok');
+            $table->integer('stok');
+            $table->integer('stok_minimal');
+            $table->string('images')->nullable();
+            $table->string('keterangan');
+            $table->boolean('is_active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
