0799a8b67d37c059cb5495fdd7c5eae0a97efc0c
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2026-06-30 22:38:57 +0700

    kategori controllers

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kategori;
+use Illuminate\Http\Request;
+use illuminate\Support\Str;
+
+class KategoriController extends Controller
+{
+    public function index(){
+        $kategori=Kategori::all();
+        confirmDelete('Hapus Data',' Apakah anda yakin ingin menghapus data ini?');
+        return view('kategori.index',compact('kategori'));
+    }
+
+    public function store(Request $request)
+    {
+        $id = $request->id;
+        $request->validate([
+            'nama_kategori' => 'required|unique:kategoris,nama_kategori,'.$id,
+            'deskripsi' => 'required|max:100|min:10'
+        ],
+        [
+            'nama_kategori.required' => 'Nama Kategori harus diisi',
+                'nama_kategori.unique' => 'Nama Kategori sudah ada',
+                'deskripsi.required' => 'Deskripsi harus diisi',
+                'deskripsi.max' => 'Deskripsi maksimal 100 karakter',
+                'deskripsi.min' => 'Deskripsi minimal 10 karekter',
+        ]);
+        // dd($request->all());
+        Kategori::updateOrCreate(
+            ['id' => $id],
+            [
+                'nama_kategori' => $request->nama_kategori,
+                'slug' => Str::slug($request->nama_kategori),
+                'deskripsi' => $request->deskripsi,
+            ]
+        );
+
+        toast()->success('Data berhasil disimpan');
+        return redirect()->route('master-data.kategori.index');
+    }
+
+    public function destroy(String $id)
+    {
+        $kategori = Kategori::findOrFail($id);
+        $kategori->delete();
+        toast()->success('Data Berhasil Dihapus');
+        return redirect()->route('master-data.kategori.index');
+    }
+}
