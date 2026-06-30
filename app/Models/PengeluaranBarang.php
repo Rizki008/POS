95f9ba076149f280e58dcfc5c6e575e941772fdd
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2026-06-30 22:43:19 +0700

    pengeluaran models

---
+++ app/Models/PengeluaranBarang.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PengeluaranBarang extends Model
+{
+    protected $guarded = ['id'];
+
+    public static function nomorPengeluaran(){
+        $maxId =self::max('id');
+        $prefix ='TRX-';
+        $nomor = $prefix . date('dmy').str_pad($maxId+1,5,'0',STR_PAD_LEFT);
+        return $nomor;
+    }
+
+    public function items()
+    {
+        return $this->hasMany(ItemPengeluaranBarang::class, 'nomor_pengeluaran','nomor_pengeluaran');
+    }
+}
